c9f093af86fcc369c7f1bc87c7bc3611a1d99d3f
Sigthorandri <sigthorandrii@gmail.com>
2019-09-05 10:30:15 +0000

    test

--- max_int.py
+++ max_int.py
@@ -1,1 +1,11 @@
-print('Hello')
+import math
+
+n = 1
+max_int = 0
+while n > 0:
+    num_int = int(input("Input a number: ")) 
+    if num_int > max_int: # Athuga hvort inntak sé stærra en það núverandi max_int
+        max_int =+ num_int # Breyta max_int í inntakið 
+    if num_int < 0: # Athuga hvort inntak sé minna en 0
+        n = -2 # Hætta í while loopu
+print("The maximum is", max_int)
